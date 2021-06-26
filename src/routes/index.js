6e9acd8bfc93da57e4272a7486a9d2b4f098b1b3
Trần Hữu Toàn <jungleoffice02@gmail.com>
2021-06-26 18:33:56 +0700

    You can now subscribe to the blog

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,9 @@ var blogRouter = require("./Blog");
 var siteRouter = require("./site");
 var aboutRouter = require("./about");
 var commentRouter = require("./comment.js");
-var mailRouter = require("./Mail");
 
+const nodemailer = require("nodemailer");
 function route(app) {
-  app.use("/mail", mailRouter);
-
   app.use("/auth", authRouter);
 
   app.use("/me", meRouter);
@@ -18,6 +16,47 @@ function route(app) {
   app.use("/comment", commentRouter);
 
   app.use("/about", aboutRouter);
+
+  app.use("/mail", (req, res) => {
+    const output = `
+      <h3>Hi ${req.body.name}</h3>
+      <p>Chào mừng bạn đến với blog! Mọi thông tin mới nhất về blog sẽ được gửi tới bạn thông qua địa chỉ email này.</p>
+      <h3>My Contact: </h3>
+      <ul>
+        <li>Email: huutrantoan@gmail.com</li>
+      </ul>
+      <br>
+      <p>Trần Hữu Toàn</p>
+      <p style="color: gray">Blog HToan</p>
+      `;
+    let sendMail = function () {
+      let mailTransporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "bloghtoan@gmail.com",
+          pass: "bloghtoan2002",
+        },
+      });
+
+      let mailDetails = {
+        from: "bloghtoan@gmail.com",
+        to: req.body.mail,
+        subject: "Thank You For Subscribing To My Blog",
+        text: "Node.js testing mail",
+        html: output,
+      };
+
+      mailTransporter.sendMail(mailDetails, function (err, data) {
+        if (err) {
+          console.log("Error Occurs");
+        } else {
+          console.log("Email sent successfully");
+        }
+      });
+    };
+    res.redirect("/");
+    sendMail();
+  });
 
   app.use("/", siteRouter);
 
