00d50692a7c55efdf3cc2ac86cdf7cd1f8824f20
Trần Hữu Toàn <jungleoffice02@gmail.com>
2021-06-27 10:54:37 +0700

    .env

--- src/routes/index.js
+++ src/routes/index.js
@@ -31,9 +31,7 @@ function route(app) {
       `;
     let sendMail = function () {
       let mailTransporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        service: "gmail",
         auth: {
           user: process.env.EMAIL,
           pass: process.env.PASSWORD,
