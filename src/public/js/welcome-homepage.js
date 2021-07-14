2a69bd24e934b68ad8029dffde6134ec2aa2394f
htoan <huutrantoan@gmail.com>
2021-07-14 18:20:53 +0700

    Fix

--- src/public/js/welcome-homepage.js
+++ src/public/js/welcome-homepage.js
@@ -3,5 +3,5 @@ let welcomeHomepage = document.querySelector(".good-morning");
 if (nowHourHp >= 0 && nowHourHp <= 11) {
   welcomeHomepage.innerHTML = "Good Morning";
 } else if (nowHourHp > 11 && nowHourHp <= 18) {
-  hello.innerHTML = "Good Afternoon";
+  welcomeHomepage.innerHTML = "Good Afternoon";
 } else welcomeHomepage.innerHTML = "Good Evening";
